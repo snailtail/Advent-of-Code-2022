632e6bd6b7f78cdeefb171fae19727ed867ca313
Magnus Olofsson <magnusolofsson@outlook.com>
2022-12-01 17:41:49 +0100

    Rewrote using map and sum

--- day01/day01.py
+++ day01/day01.py
@@ -1,22 +1,16 @@
 with open('day01/day01_input.txt', 'r') as f:
-    lines = f.readlines()
+    calories = f.read()
 
 # List will store the sums of each elfs calories
 elfs=[]
-calorie_sum=0
-for line in lines:
-    if line.rstrip()=="":
-        # We hit a separator line - the next line will belong to a new elf
-       elfs.append(calorie_sum) # Store this elfs total calories
-       calorie_sum=0            # Reset for next elf
-    else:
-        calorie_sum+= int(line.rstrip()) # Just add to the current elfs total
+for elf in calories.split('\n\n'):
+    elfs.append(sum(list(map(int,elf.split()))))
 
 # Sort the list in descending order
 elfs.sort(reverse=True)
 
 # Step 1 - The max calorie count for an elf
-print(max(elfs))
+print(elfs[0])
 
-# Step 2 - The sum of the max calorie count for the top three elfs.
+# Step 2 - The sum of the top three calorie counts
 print(sum(elfs[0:3]))
