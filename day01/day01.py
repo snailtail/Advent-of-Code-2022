fd6e58b7d2fa667a7e3746c0975ac9bdc79d9ae5
Magnus Olofsson <magnusolofsson@outlook.com>
2022-12-01 14:34:23 +0100

    Clean up Day 1

--- day01/day01.py
+++ day01/day01.py
@@ -1,18 +1,18 @@
-with open('day01/day01_input.txt') as f:
+with open('day01/day01_input.txt', 'r') as f:
     lines = f.readlines()
 
-# Create a list with some empty elfs - index 0 will not be used, and index 1 will start with 0 to be added upon
-elfs=[0,0]
-index=1
+# List will store the sums of each elfs calories
+elfs=[]
+calorie_sum=0
 for line in lines:
     if line.rstrip()=="":
-        # we hit a separator line - the next line will belong to a new elf
-       index+=1
-       elfs.append(0) # start out with a 0 value for the new elf
+        # We hit a separator line - the next line will belong to a new elf
+       elfs.append(calorie_sum) # Store this elfs total calories
+       calorie_sum=0            # Reset for next elf
     else:
-        elfs[index]+= int(line.rstrip())
+        calorie_sum+= int(line.rstrip()) # Just add to the current elfs total
 
-#sort the list in descending order
+# Sort the list in descending order
 elfs.sort(reverse=True)
 
 # Step 1 - The max calorie count for an elf
