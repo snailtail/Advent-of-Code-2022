711ed1144a3161bedaefad1f925b56fbc571ca98
Magnus Olofsson <magnusolofsson@outlook.com>
2022-12-03 12:15:14 +0100

    Clean up

--- day03/day03.py
+++ day03/day03.py
@@ -13,15 +13,18 @@ def in_all_three(l1, l2, l3):
     # Checks each character in l1 and returns it, if it's present in l2 and l3 as well.
     for character in l1:
         if character in l2 and character in l3:
-            print(character)
             return character
 
 def getValue(n):
-    # Get the value for each item type:
+    # Get the value for each item type, using ord to retrieve the ASCII value
+    # and just subtracting so we end up in the specified ranges:
+    # Lowercase item types a through z have priorities 1 through 26.
+    # Uppercase item types A through Z have priorities 27 through 52.
+    
     if n.islower():
-        return ord(n)-96
+        return ord(n)-96 # lowercase a is 97 and should be 1
     else:
-        return ord(n)-38
+        return ord(n)-38 # Uppercase A is 65 and should be 27
     
 
 
