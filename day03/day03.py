77f0cca293765b70d79a8890a2b0d009622e94ff
Magnus Olofsson <magnusolofsson@outlook.com>
2022-12-03 08:27:11 +0100

    Solved step 2

--- day03/day03.py
+++ day03/day03.py
@@ -8,7 +8,14 @@ def pairs(line):
     for character in comp1:
         if character in comp2:
             return character
-    
+
+def in_all_three(l1, l2, l3):
+    # Checks each character in l1 and returns it, if it's present in l2 and l3 as well.
+    for character in l1:
+        if character in l2 and character in l3:
+            print(character)
+            return character
+
 def getValue(n):
     # Get the value for each item type:
     if n.islower():
@@ -24,6 +31,13 @@ def main():
     for item in data:
         step1_sum += getValue(pairs(item))
     print(step1_sum)
+    
+    step2_sum=0
+    # Loop over the list in chuncks of three
+    for index in range(2,len(data),3):
+        step2_sum += getValue(in_all_three(data[index],data[index-1],data[index-2]))
+    print(step2_sum)
+    
 
 if __name__=="__main__":
     main()
