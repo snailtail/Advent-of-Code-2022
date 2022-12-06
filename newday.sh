0b4ad585d7284863f42e76c14795bfbd4dbeb10e
Magnus Olofsson <magnusolofsson@outlook.com>
2022-12-06 06:16:14 +0100

    Solved day 6

--- newday.sh
+++ newday.sh
@@ -3,9 +3,10 @@ git checkout main
 git pull -r
 git checkout -b day$1
 mkdir day$1
+python3 ./getinput.py $1
 cd day$1
 touch day$1.py
-touch day$1_input.txt
+#touch day$1_input.txt
 touch day$1_testinput.txt
 echo "# :christmas_tree: Advent of Code 2022 Day$1 :christmas_tree:" > README.md
 git add .
