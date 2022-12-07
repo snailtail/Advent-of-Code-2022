e7f384a2971a227ad68f9bb5d1c7e82d92aff6e9
Magnus Olofsson <magnusolofsson@outlook.com>
2022-12-07 13:34:27 +0100

    Clean up

--- day07/day07.py
+++ day07/day07.py
@@ -9,10 +9,11 @@ def extract_sizes(command):
 with open('day07/day07_input.txt', 'r') as file:
     lines = file.read()
 
+# Using defaultdict for ease of use - if the key does not exist, we will not get a key-error, but instead add a new item with default value 0.
 dir_sizes = defaultdict(lambda : 0)
 dir_tree = []
 
-# Splitta på "$ cd" för att hitta alla actions som gör efter att man byter current workdir. Splitta sen detta på newlines, och leta fram alla numeriska värden som finns gömda i dessa.
+# Split by "$ cd" to find all actions after changing the current workdir. Then split on newlines, and find all numeric values within.
 for directory, size in [(command.split('\n')[0], sum(extract_sizes(command))) for command in lines.split('$ cd') if command != '']: 
     directory = directory.strip()
     if directory == '..':
@@ -25,14 +26,14 @@ for directory, size in [(command.split('\n')[0], sum(extract_sizes(command))) fo
         dir_sizes[cur_dir] += size
 
 # Step 1
-# Summera alla som är under tröskeln
+# Sum of all which are below the threshold 100000
 step1_sum=sum([size for directory, size in dir_sizes.items() if size < 100000])
 print('Step 1: ', step1_sum)
 
 
 
 # Step 2
-# Hitta den minsta av de som är tillräckligt stora för att frigöra det önskade utrymmet
+# Find the smallest of those who are big enough to free up the desired space
 space_used = dir_sizes['//']
 space_to_delete = 30000000 - (70000000 - space_used)
 step2=min([size for directory, size in dir_sizes.items() if size >= space_to_delete])
