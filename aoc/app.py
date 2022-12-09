9b96d64f7637846e8e7863a075e1105e252801c4
Magnus Olofsson <magnusolofsson@outlook.com>
2022-12-09 05:57:27 +0100

    AoC Modules

--- aoc/app.py
+++
@@ -1,6 +0,0 @@
-import map
-
-test = map.FourWayMap()
-test.Move("Up 2")
-print(test.coordinate())
-print(test.CalculateManhattanDistance("3:-1"))
--- aoc/map.py
+++ aoc/map.py
@@ -1,65 +1,67 @@
 import re
 class FourWayMap():
-        x = 0
-        y = 0
         
         def coordinate(self):
             return f"{self.x}:{self.y}"
         
-        visitedCoordinates = set()
-        originCoordinate=""
 
         def __init__(self, X=0, Y=0):
+            self.visitedCoordinates = set()
+            self.originCoordinate=""
             self.x = X
-            self.y = Y;
-            self.originCoordinate = self.coordinate()
+            self.y = Y
+            self.originCoordinate = f"{self.x}:{self.y}"
 
         #region move_around
 
         def MoveUp(self, Steps):
             for i  in range(Steps):
                 self.y+=1
-                self.visitedCoordinates.add(self.coordinate())
+                self.visitedCoordinates.add(f"{self.x}:{self.y}")
         
         def MoveDown(self, Steps):
             for i  in range(Steps):
                 self.y-=1
-                self.visitedCoordinates.add(self.coordinate)
+                self.visitedCoordinates.add(f"{self.x}:{self.y}")
 
         def MoveRight(self, Steps):
             for i  in range(Steps):
                 self.x+=1
-                self.visitedCoordinates.add(self.coordinate)
+                self.visitedCoordinates.add(f"{self.x}:{self.y}")
 
         def MoveLeft(self, Steps):
             for i  in range(Steps):
                 self.x-=1
-                self.visitedCoordinates.add(self.coordinate)
+                self.visitedCoordinates.add(f"{self.x}:{self.y}")
 
         #endregion
 
         #region parse input
         def Move(self, MoveInstruction):
+            if type(MoveInstruction)==list:
+                moves = MoveInstruction
+            else:
+                moves = [MoveInstruction]
             
+            for move in moves:
+                direction = '?'
+                steps = 0
+                match = re.match(r"(\D+)(\d+)", move.lower())
+                if match != None:
+                    direction = match[1].rstrip()
+                    steps = int(match[2])
 
-            direction = '?'
-            steps = 0
-            match = re.match(r"(\D+)(\d+)", MoveInstruction.lower())
-            if match != None:
-                direction = match[1].rstrip()
-                steps = int(match[2])
-
-            
-            if direction in ["n","u","up","north"]:
-                direction = "n"
-            elif direction in ["s","d","down","south"]:
-                direction = "s"
-            elif direction in ["e","r","east","right"]:
-                direction = "e"
-            elif direction in ["w","l","west","left"]:
-                direction = "w"
-            
-            self.DoMove(direction, steps)
+                
+                if direction in ["n","u","up","north"]:
+                    direction = "n"
+                elif direction in ["s","d","down","south"]:
+                    direction = "s"
+                elif direction in ["e","r","east","right"]:
+                    direction = "e"
+                elif direction in ["w","l","west","left"]:
+                    direction = "w"
+                
+                self.DoMove(direction, steps)
 
         def DoMove(self, Direction, Steps):
             
