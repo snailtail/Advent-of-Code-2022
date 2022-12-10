348aa2992e9f844aca22070666acca94aaf98524
Magnus Olofsson <magnusolofsson@outlook.com>
2022-12-10 23:21:25 +0100

    Add new day 10

---
+++ 10.md
@@ -0,0 +1,1 @@
+# :christmas_tree: Advent of Code 2022 Day10 :christmas_tree:
--- newday.sh
+++ newday.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-git checkout main
-git pull -r
-git checkout -b day$1
-mkdir day$1
+
+#git checkout main
+#git pull -r
+#git checkout -b day$1
+
+# mkdir day$1
 python3 ./getinput.py $1
-cd day$1
-touch day$1.py
-#touch day$1_input.txt
-touch day$1_testinput.txt
-echo "# :christmas_tree: Advent of Code 2022 Day$1 :christmas_tree:" > README.md
+#cd day$1
+touch $1.py
+touch $1test.dat
+echo "# :christmas_tree: Advent of Code 2022 Day$1 :christmas_tree:" > $1.md
 git add .
 git commit -m "Add new day $1"
 git push --set-upstream origin day$1
