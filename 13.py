fafdbba9afa1963c9947720e7e16302ce4f683b7
Magnus Olofsson <magnusolofsson@outlook.com>
2022-12-13 16:03:52 +0100

    Solve day 13 step 2

--- 13.py
+++ 13.py
@@ -1,3 +1,5 @@
+from functools import cmp_to_key
+
 from aoc import inpututil as iu
 import os
 
@@ -46,6 +48,10 @@ def compare(part1,part2):
 
 
 def step1(input):
+    """
+        Splits the data into pairs. And uses the compare() method to see if part1 of the pair is smaller than part2
+        Prints out the sum of the 1-based indices for the list where part1 is smaller than part 2 (correctly sorted).
+    """
     result = 0
     for i, group in enumerate(input.strip().split('\n\n')):
         pair1, pair2 = group.split('\n')
@@ -57,10 +63,46 @@ def step1(input):
     print(f"Step 1 {result}")
 
 
+def step2(input):
+    
+    """
+        Splits the data, but ignores pairs and just adds all the packets to a large list.
+        Additionally adds two divider packets to the list.
+        Sorts the list using the compare() method, with the help of cmp_to_key() from functools.
+        Prints the product of the (1-based) indices where the divider packets end up.
+    """
+    orderedpackets=[]
+
+    # Append the special divider packets
+    orderedpackets.append([[2]])
+    orderedpackets.append([[6]])
+    # Append the packets from the input
+    for i, group in enumerate(input.strip().split('\n\n')):
+        pair1, pair2 = group.split('\n')
+        pair1 = eval(pair1)
+        pair2 = eval(pair2)    
+        orderedpackets.append(pair1)
+        orderedpackets.append(pair2)
+
+    # Sorting can apparently be done using the cmp_to_key function from functools
+    # It compares each element with every other element until a sorted list is obtained
+    # And calls a separate method which does the actual comparison, which is perfect since we already built the compare method!
+    orderedpackets = sorted(orderedpackets,key=cmp_to_key(lambda p1,p2: compare(p1,p2)))
+    #print(orderedpackets)
+
+    # Last piece of the solution for step 2 is to find the divider packets
+    result=1 # start at 1 to manage the multiplications
+    for index,packet in enumerate(orderedpackets):
+        if packet==[[2]] or packet==[[6]]: # we found a divider packet
+            result = result * (index+1) # 1-based index
+    
+    print(f"Step 2: {result}")
+
 
 def main():
     data = util.GetContents(file, test=False)
     step1(data)
+    step2(data)
 
 if __name__=="__main__":
     main()
--- README.md
+++ README.md
@@ -43,5 +43,7 @@ So I woke up to Graphs this morning, and wanted to go straight back to bed. But 
 
 - [- Day 13: Distress Signal -](./13.md)
 Holy snailfish dejavu Batman! Oh no... Not this again, I gave up on this last year. But as the day passes I start thinking that perhaps Pythons `eval()` is my friend? Going to try for step 1 at least.
-`eval()` really helped to parse the input into something manageable! Step 1 was challenging to get the logic correct. In the end it became some morphed recusion thingy of a method that performs the actual comparisons.
+`eval()` really helped to parse the input into something manageable! For step 1 it was challenging to get the comparison logic correct. In the end it became some morphed recusion thingy of a method that performs the actual comparisons.
 For step 2 it gets a lot worse, how the heck do you sort that entire list...? I wish I had some sort of way (hehe, pun intended) to use the `compare()` method for sorting..!
+The documentation for `sorted()` actually pointed me towards the cmp_to_key function from functools which took me a while to figure out how to implement, since I'm not used to the whole lamdba thing yet.
+In the end I spent a lot of time forgetting that multiplying a value by 0 doesn't do much for increasing the value... But after actually __using my eyes__ I found that error and it seems to work now both for the test input and for the actual puzzle input.
