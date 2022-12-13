651d663176b9a8cd15417e25759a217122be77c9
Magnus Olofsson <magnusolofsson@outlook.com>
2022-12-13 10:39:16 +0100

    Start day 13

--- newday.sh
+++ newday.sh
@@ -6,7 +6,7 @@ git checkout -b day$1
 
 python3 ./getinput.py $1
 cp template.py $1.py
-touch $1test.dat
+touch ./data/$1test.dat
 echo "# :christmas_tree: Advent of Code 2022 Day$1 :christmas_tree:" > $1.md
 git add .
 git commit -m "Add new day $1"
